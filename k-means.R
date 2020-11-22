f56fdaa19ef0b47243c3a8587d2f7db2d6519ff1
josea <janaosea.25@gmail.com>
2020-11-22 16:55:17 -0700

    twitter scrape

--- k-means.R
+++ k-means.R
@@ -1,18 +1,19 @@
 library(factoextra)
-library(ggpubr)
+library(tidyverse)
+library(twitteR)
+library(tidytext)
 
+consumer_key <- "8JUJeufLuUlqyqYsbxFBJp7aB"
+consumer_secret <- "l1tLcsUIU753GyDuttCDT3ACKguuPGD4Lnd7JRJXHEaEy5tJu1"
+access_token <- "377520293-EvuYxzVO5aZoTtXA0otIRfqxYG6HqCEDhjuJt4BK"
+access_secret <- "LbvA7nKJY0Wx4lR8QOt7gxb1pKRajA8gJi7MtXux0nQdh"
 
+setup_twitter_oauth(consumer_key, consumer_secret, access_token, access_secret)
 
-
-df <- read.csv("gender-classifier-DFE-791531.csv")
-names(df)
-X <- na.omit(data.frame(col1 = df$gender.confidence, col2 = df$profile_yn.confidence))
-km.res <- kmeans(X, 2, nstart=25)
-km.res$cluster
-
-fviz_cluster(km.res, data = X,
-             palette = c("#2E9FDF", "#00AFBB", "#E7B800"), 
-             geom = "point",
-             ellipse.type = "convex", 
-             ggtheme = theme_bw()
-             )
+fn_twitter <- searchTwitter("#Houston",n=1000,lang="en")
+fn_twitter_df <- twListToDF(fn_twitter)
+tweet_words <- fn_twitter_df %>% select(id, text) %>% unnest_tokens(word,text)
+tweet_words %>% count(word,sort=T) %>% slice(1:20) %>% 
+  ggplot(aes(x = reorder(word, 
+                         n, function(n) -n), y = n)) + geom_bar(stat = "identity") + theme(axis.text.x = element_text(angle = 60, 
+                                                                                                                      hjust = 1)) + xlab("")
