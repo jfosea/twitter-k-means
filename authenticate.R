2d26dfdc270d4725ca38f9a676924cb3d3b1a33d
josea <janaosea.25@gmail.com>
2020-11-23 02:12:24 -0700

    hide keys

--- authenticate.R
+++ authenticate.R
@@ -1,10 +1,10 @@
 library(twitteR)
 
 authenticate <- function() {
-  consumer_key <- "8JUJeufLuUlqyqYsbxFBJp7aB"
-  consumer_secret <- "l1tLcsUIU753GyDuttCDT3ACKguuPGD4Lnd7JRJXHEaEy5tJu1"
-  access_token <- "377520293-EvuYxzVO5aZoTtXA0otIRfqxYG6HqCEDhjuJt4BK"
-  access_secret <- "LbvA7nKJY0Wx4lR8QOt7gxb1pKRajA8gJi7MtXux0nQdh"
+  consumer_key <- "insert here"
+  consumer_secret <- "insert here"
+  access_token <- "instert here"
+  access_secret <- "insert here"
   
   setup_twitter_oauth(consumer_key, consumer_secret, access_token, access_secret)
 }
