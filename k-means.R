d4977d7e499853dcb1dd73634b07de62f5a0c6f2
Evan Quan <evanlquan@gmail.com>
2020-11-22 17:47:28 -0700

    Move authentication and add stop words

--- k-means.R
+++ k-means.R
@@ -2,18 +2,28 @@ library(factoextra)
 library(tidyverse)
 library(twitteR)
 library(tidytext)
+source("authenticate.R")
 
-consumer_key <- "8JUJeufLuUlqyqYsbxFBJp7aB"
-consumer_secret <- "l1tLcsUIU753GyDuttCDT3ACKguuPGD4Lnd7JRJXHEaEy5tJu1"
-access_token <- "377520293-EvuYxzVO5aZoTtXA0otIRfqxYG6HqCEDhjuJt4BK"
-access_secret <- "LbvA7nKJY0Wx4lR8QOt7gxb1pKRajA8gJi7MtXux0nQdh"
+# Input values
+topic <- "trump"
+number_of_tweets <- 1000
+exclude_words <- c(topic, "https", "t.co", "rt", "amp","travelban")
 
-setup_twitter_oauth(consumer_key, consumer_secret, access_token, access_secret)
+authenticate()
 
-fn_twitter <- searchTwitter("#Houston",n=1000,lang="en")
+fn_twitter <- searchTwitter(topic,n=number_of_tweets,lang="en")
 fn_twitter_df <- twListToDF(fn_twitter)
+
+
 tweet_words <- fn_twitter_df %>% select(id, text) %>% unnest_tokens(word,text)
+
+
 tweet_words %>% count(word,sort=T) %>% slice(1:20) %>% 
   ggplot(aes(x = reorder(word, 
                          n, function(n) -n), y = n)) + geom_bar(stat = "identity") + theme(axis.text.x = element_text(angle = 60, 
                                                                                                                       hjust = 1)) + xlab("")
+
+# Create a list of stop words: a list of words that are not worth including
+
+my_stop_words <- stop_words %>% select(-lexicon) %>% 
+  bind_rows(data.frame(word = exclude_words))
